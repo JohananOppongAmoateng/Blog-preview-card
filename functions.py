2b159d6267dd32b43d14190d0af5a35e38009a0b
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2024-03-10 20:58:26 +0000

    need more work

---
+++ functions.py
@@ -0,0 +1,34 @@
+# def welcome():
+#     print("Welcome pythonista to PyClub KNUST")
+
+
+# def display_admission_information(fname,lname,dob,country,programme,college,address,pname,pcontact):
+#     print(f"First Name {fname}")
+#     print(f"Last Name {lname}")
+#     print(f"Date of Birth {dob}")
+#     print(f"Country {country}")
+#     print(f"Programme {programme}")
+#     print(f"College {college}")
+#     print(f"Address {address}")
+#     print(f"Parent Name {pname}")
+#     print(f"FParent Contact {pcontact}")
+
+
+# display_admission_information("JOe","Oppong","10th March","Ghana","Computer Science","College OF Science","KOtei","Anaba",382974289743928)
+
+# def area_and_perimeter_rectangle(length,width):
+#     perimeter  = 2 * length + 2 * width
+#     area =  length * width
+#     return (perimeter,area)
+
+# def area_and_perimeter_square(length):
+#     perimeter  = 4 * length 
+#     area =  length * length
+#     return (perimeter,area)
+
+
+# print(area_and_perimeter_rectangle(4,6))
+# print(area_and_perimeter_square(8))
+
+x = lambda a : a + 10
+print(x(5))
